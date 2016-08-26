8d7bd0d66b50c9d966ef185db4f504c004ca8d4c
Orvin Welchez <owelchez@CRESTRON.CRESTRON.COM>
2016-08-26 17:48:06 -0400

    Questions

--- seeders/answers.sql
+++ seeders/answers.sql
@@ -1,299 +1,299 @@
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Countess Marya Zaleska", false, 1)
+VALUES ("Countess Marya Zaleska", false, 1);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Tiffany", false, 1)
+VALUES ("Tiffany", false, 1);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Bride of Frankenstein", true, 1)
+VALUES ("Bride of Frankenstein", true, 1);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Lily Munster", false, 1)
+VALUES ("Lily Munster", false, 1);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Freddie Kruger", true, 2)
+VALUES ("Freddie Kruger", true, 2);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Gerard Demus", false, 2)
+VALUES ("Gerard Demus", false, 2);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Bubba Sawyer", false, 2)
+VALUES ("Bubba Sawyer", false, 2);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Stu Matcher", false, 2)
+VALUES ("Stu Matcher", false, 2);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("1983", false, 3)
+VALUES ("1983", false, 3);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("1994", false, 3)
+VALUES ("1994", false, 3);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("1979", true, 3)
+VALUES ("1979", true, 3);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("1967", false, 3)
+VALUES ("1967", false, 3);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("The Fog", false, 4)
+VALUES ("The Fog", false, 4);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("I Know What You Did Last Summer", false, 4)
+VALUES ("I Know What You Did Last Summer", false, 4);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Scream", false, 4)
+VALUES ("Scream", false, 4);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Texas Chainsaw Massacre", true, 4)
+VALUES ("Texas Chainsaw Massacre", true, 4);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("George Clooney", true, 5)
+VALUES ("George Clooney", true, 5);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Brendan Frasier", false, 5)
+VALUES ("Brendan Frasier", false, 5);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Quentin Tarantino", false, 5)
+VALUES ("Quentin Tarantino", false, 5);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Gerard Butler", false, 5)
+VALUES ("Gerard Butler", false, 5);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("New Nightmare", false, 6)
+VALUES ("New Nightmare", false, 6);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Ginger Snaps", true, 6)
+VALUES ("Ginger Snaps", true, 6);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Red Eye", false, 6)
+VALUES ("Red Eye", false, 6);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Scream", false, 6)
+VALUES ("Scream", false, 6);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Underworld", false, 7)
+VALUES ("Underworld", false, 7);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Blood and Chocolate", false, 7)
+VALUES ("Blood and Chocolate", false, 7);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("One Dark Night", true, 7)
+VALUES ("One Dark Night", true, 7);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Wild Country", false, 7)
+VALUES ("Wild Country", false, 7);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Johnson", false, 8)
+VALUES ("Johnson", false, 8);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Williams", false, 8)
+VALUES ("Williams", false, 8);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Harker", true, 8)
+VALUES ("Harker", true, 8);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Roberts", false, 8)
+VALUES ("Roberts", false, 8);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Is a demon", false, 9)
+VALUES ("Is a demon", false, 9);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Personality change", false, 9)
+VALUES ("Personality change", false, 9);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Invisibility", true, 9)
+VALUES ("Invisibility", true, 9);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Turns into a werewolf", false, 9)
+VALUES ("Turns into a werewolf", false, 9);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("The Guardian", false, 10)
+VALUES ("The Guardian", false, 10);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Phone Booth", false, 10)
+VALUES ("Phone Booth", false, 10);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Hide and Seek", true, 10)
+VALUES ("Hide and Seek", true, 10);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("The Skeleton Key", false, 10)
+VALUES ("The Skeleton Key", false, 10);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Silent Night, Deadly Night", false, 11)
+VALUES ("Silent Night, Deadly Night", false, 11);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Blood Wedding", true, 11)
+VALUES ("Blood Wedding", true, 11);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("The Ugly", false, 11)
+VALUES ("The Ugly", false, 11);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Bleed", false, 11)
+VALUES ("Bleed", false, 11);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Zombies", false, 12)
+VALUES ("Zombies", false, 12);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Dogs", true, 12)
+VALUES ("Dogs", true, 12);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Aliens", false, 12)
+VALUES ("Aliens", false, 12);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Werewolves", false, 12)
+VALUES ("Werewolves", false, 12);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("The Incubus", false, 13)
+VALUES ("The Incubus", false, 13);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Hellraiser", true, 13)
+VALUES ("Hellraiser", true, 13);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Cursed", false, 13)
+VALUES ("Cursed", false, 13);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("The Unseen", false, 13)
+VALUES ("The Unseen", false, 13);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("San Francisco", false, 14)
+VALUES ("San Francisco", false, 14);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("The Florida Keys", false, 14)
+VALUES ("The Florida Keys", false, 14);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Long Island", true, 14)
+VALUES ("Long Island", true, 14);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Houston", false, 14)
+VALUES ("Houston", false, 14);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Anthony Perkins", false, 15)
+VALUES ("Anthony Perkins", false, 15);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Jamie Lee Curtis", false, 15)
+VALUES ("Jamie Lee Curtis", false, 15);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Norman Bates", false, 15)
+VALUES ("Norman Bates", false, 15);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Janet Leigh", true, 15)
+VALUES ("Janet Leigh", true, 15);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Bel Air Motel", false, 16)
+VALUES ("Bel Air Motel", false, 16);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Slumberland", false, 16)
+VALUES ("Slumberland", false, 16);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Bates Motel", true, 16)
+VALUES ("Bates Motel", true, 16);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Crave Inn", false, 16)
+VALUES ("Crave Inn", false, 16);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Ohio", false, 17)
+VALUES ("Ohio", false, 17);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Indiana", false, 17)
+VALUES ("Indiana", false, 17);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("California", false, 17)
+VALUES ("California", false, 17);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Illinois", true, 17)
+VALUES ("Illinois", true, 17);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Zombieland", false, 18)
+VALUES ("Zombieland", false, 18);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Final Destination", false, 18)
+VALUES ("Final Destination", false, 18);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Halloween H20", false, 18)
+VALUES ("Halloween H20", false, 18);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Scream 4", true, 18)
+VALUES ("Scream 4", true, 18);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("The Lakeshore Strangler", false, 19)
+VALUES ("The Lakeshore Strangler", false, 19);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("The Lincoln Park Loon", false, 19)
+VALUES ("The Lincoln Park Loon", false, 19);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("The North Shore Killer", false, 19)
+VALUES ("The North Shore Killer", false, 19);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("The South Side Psycho", false, 19)
+VALUES ("The South Side Psycho", false, 19);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Robert Rodriguez", false, 20)
+VALUES ("Robert Rodriguez", false, 20);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Eli Roth", false, 20)
+VALUES ("Eli Roth", false, 20);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("James Wan", true, 20)
+VALUES ("James Wan", true, 20);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Quentin Tarantino", false, 20)
+VALUES ("Quentin Tarantino", false, 20);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Ireland", false, 21)
+VALUES ("Ireland", false, 21);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Japan", true, 21)
+VALUES ("Japan", true, 21);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Australia", false, 21)
+VALUES ("Australia", false, 21);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Indonesia", false, 21)
+VALUES ("Indonesia", false, 21);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("12", true, 22)
+VALUES ("12", true, 22);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("7", false, 22)
+VALUES ("7", false, 22);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("9", false, 22)
+VALUES ("9", false, 22);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("5", false, 22)
+VALUES ("5", false, 22);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Fireman", false, 23)
+VALUES ("Fireman", false, 23);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Mechanic", false, 23)
+VALUES ("Mechanic", false, 23);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("College Professor", true, 23)
+VALUES ("College Professor", true, 23);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Architect", false, 23)
+VALUES ("Architect", false, 23);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Kruger", false, 24)
+VALUES ("Kruger", false, 24);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Best Buy", false, 24)
+VALUES ("Best Buy", false, 24);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Foree", false, 24)
+VALUES ("Foree", false, 24);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Target", false, 24)
+VALUES ("Target", false, 24);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Ted Bundy", false, 25)
+VALUES ("Ted Bundy", false, 25);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("The Gainesville Ripper", true, 25)
+VALUES ("The Gainesville Ripper", true, 25);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("Charles Manson", false, 25)
+VALUES ("Charles Manson", false, 25);
 
 INSERT INTO answers (text, status, QuestionID)
-VALUES ("The Boston Strangler", false, 25)
+VALUES ("The Boston Strangler", false, 25);
