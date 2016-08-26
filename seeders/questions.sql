06ecfc85ad97afa278db5561f338f758f4d83490
Orvin Welchez <owelchez@CRESTRON.CRESTRON.COM>
2016-08-26 17:43:13 -0400

    Questions

--- seeders/questions.sql
+++ seeders/questions.sql
@@ -1,74 +1,74 @@
 INSERT INTO questions (text, cAnswerID)
-VALUES ("1. Who was the first female monster to appear in a movie? ", 1) 
+VALUES ("1. Who was the first female monster to appear in a movie? ", 1); 
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("2. Which of these killers would you have to fear if you fell asleep? ", 2)
+VALUES ("2. Which of these killers would you have to fear if you fell asleep? ", 2);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("3. In what year did the original version of When A Stranger Calls debut? ", 3)
+VALUES ("3. In what year did the original version of When A Stranger Calls debut? ", 3);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("4. Completely under the control of his family, Bubba Sawyer was the main antagonist in which of these horror classics? ", 4)
+VALUES ("4. Completely under the control of his family, Bubba Sawyer was the main antagonist in which of these horror classics? ", 4);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("5. Released in 1996, who played Seth Gecko in the hit comedy horror film From Dusk Till Dawn? ", 5)
+VALUES ("5. Released in 1996, who played Seth Gecko in the hit comedy horror film From Dusk Till Dawn? ", 5);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("6. Which of these horror films was not directed by Wes Craven? ", 6)
+VALUES ("6. Which of these horror films was not directed by Wes Craven? ", 6);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("7. Which of these horror flicks does not feature werewolves? ", 7)
+VALUES ("7. Which of these horror flicks does not feature werewolves? ", 7);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("8. In the remade 2008 movie Its Alive, what is the last name of the mother? ", 8)
+VALUES ("8. In the remade 2008 movie Its Alive, what is the last name of the mother? ", 8);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("9. In the 2000 movie Hollow Man, which ability does Sebastian Caine possess? ", 9)
+VALUES ("9. In the 2000 movie Hollow Man, which ability does Sebastian Caine possess? ", 9);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("10. Directed by John Polson, which of these horror movies stars Robert De Niro as Dr. David Callaway? ", 10)
+VALUES ("10. Directed by John Polson, which of these horror movies stars Robert De Niro as Dr. David Callaway? ", 10);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("11. Released in 1980, what is another title for the slasher movie He Knows You're Alone? ", 11)
+VALUES ("11. Released in 1980, what is another title for the slasher movie He Knows You're Alone? ", 11);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("12. In the 2006 horror film The Breed, what entity is the killer? ", 12)
+VALUES ("12. In the 2006 horror film The Breed, what entity is the killer? ", 12);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("13. Released in 1987, which horro flick used the tagline 'Satan's done waiting'? ", 13)
+VALUES ("13. Released in 1987, which horro flick used the tagline 'Satan's done waiting'? ", 13);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("14. Where is Amityville? ", 14)
+VALUES ("14. Where is Amityville? ", 14);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("15. Halloween H20 features a cast member from Psycho. Who is it? ", 15)
+VALUES ("15. Halloween H20 features a cast member from Psycho. Who is it? ", 15);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("16. What is the name of the motel in Psycho? ", 16)
+VALUES ("16. What is the name of the motel in Psycho? ", 16);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("17. Halloween primarily takes place in which state? ", 17)
+VALUES ("17. Halloween primarily takes place in which state? ", 17);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("18. The characters in this horror movie are seen watching Shaun of The Dead. Name the movie. ", 18)
+VALUES ("18. The characters in this horror movie are seen watching Shaun of The Dead. Name the movie. ", 18);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("19. What is the name of the serial killer at the start of Child's Play? ", 19)
+VALUES ("19. What is the name of the serial killer at the start of Child's Play? ", 19);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("20. Who directed the first Saw film? ", 20)
+VALUES ("20. Who directed the first Saw film? ", 20);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("21. The Ring is originally based on a 1998 horror movie from which country? ", 21)
+VALUES ("21. The Ring is originally based on a 1998 horror movie from which country? ", 21);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("22. How many Friday The 13th movies are there? ", 22)
+VALUES ("22. How many Friday The 13th movies are there? ", 22);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("23. Before becoming a director, Wes Craven was originally a ... ", 23)
+VALUES ("23. Before becoming a director, Wes Craven was originally a ... ", 23);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("24. What is the name of the electronics store Shaun works at in Shaun of The Dead? ", 24)
+VALUES ("24. What is the name of the electronics store Shaun works at in Shaun of The Dead? ", 24);
 
 INSERT INTO questions (text, cAnswerID)
-VALUES ("25. Kevin Williamson was inspired to write Scream after watching a documentary about which real-life serial killer? ", 25)
+VALUES ("25. Kevin Williamson was inspired to write Scream after watching a documentary about which real-life serial killer? ", 25);
